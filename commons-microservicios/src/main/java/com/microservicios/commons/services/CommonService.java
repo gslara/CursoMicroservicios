39ec10ac119b7ae9d570e988a0847c1a97eba67b
Sofía Lara <sofilara147@gmail.com>
2021-02-16 18:54:00 -0300

    Sección 7 - backend paginación

--- commons-microservicios/src/main/java/com/microservicios/commons/services/CommonService.java
+++ commons-microservicios/src/main/java/com/microservicios/commons/services/CommonService.java
@@ -2,9 +2,14 @@ package com.microservicios.commons.services;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface CommonService<E> {
 
 	public Iterable<E> findAll();
+	
+	public Page<E> findAll(Pageable pageable); //pasamos Pageable como parámetro para que sea paginable. Page es un Iterable por rangos, para paginar
 	
 	public Optional<E> findById(Long id); //usamos Optional para evitar errores si lo que trae es un null
 	
--- commons-microservicios/src/main/java/com/microservicios/commons/services/CommonServiceImpl.java
+++ commons-microservicios/src/main/java/com/microservicios/commons/services/CommonServiceImpl.java
@@ -3,10 +3,12 @@ package com.microservicios.commons.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-public class CommonServiceImpl<E, R extends CrudRepository<E, Long>> implements CommonService<E> { //Hay que crear un dato (R= repository), interfaz, que herede de CrudRepository
+public class CommonServiceImpl<E, R extends PagingAndSortingRepository<E, Long>> implements CommonService<E> { //Hay que crear un dato (R= repository), interfaz, que herede de CrudRepository
 
 	//Inyección del repositorio ----------------------
 	@Autowired
@@ -36,6 +38,12 @@ public class CommonServiceImpl<E, R extends CrudRepository<E, Long>> implements 
 	@Transactional
 	public void deleteById(Long id) {
 		repository.deleteById(id);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Page<E> findAll(Pageable pageable) {
+		return repository.findAll(pageable);
 	}
 
 }
